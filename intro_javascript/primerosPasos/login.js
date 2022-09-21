062fdb9399d1f1f7ec833102f7edebbc590001d9
cchavezmx <cchavezmx@outlook.com>
2022-09-21 02:37:21 +0000

    nueva clase de javascript

---
+++ intro_javascript/primerosPasos/login.js
@@ -0,0 +1,1 @@
+//funcion para loguearse() 
---
+++ intro_javascript/primerosPasos/main.js
@@ -0,0 +1,55 @@
+// import exportar funciones
+// escritos para ustedes
+// antes de comentar crear funcion y variables que sean intuitivas
+
+
+// el alert es una funcion que muestra un mensaje en pantalla
+// detiene el hilo de ejecucion hasta que el usuario presiona OK
+// alert("Hola desde hmtl en un archivo de javascript");
+
+
+//  2 tipos de datos
+//  1 -primitivos
+
+// tipos de datos primitivo
+
+// string
+// cadena de texto
+"Esta es mi cadena de texto"
+'Esta es otra cadena de texto'
+"Probando otra cadena 'de' text"
+
+// numeros
+// integer
+// 10 20 30 40 50
+// float
+// 1.5 0.5 
+
+//  Boolean
+// true false
+
+// null
+// la varible tiene asignado un valor nullo
+let usuariosAnios = null
+console.log(usuariosAnios)
+
+// undefined
+// no exite un valor asinado a la variable
+let unsuariosNuevos
+console.log(unsuariosNuevos)
+
+
+// let, const ---> var ***
+
+// como declarar variables
+let nombre = "Carlos"
+console.log(nombre)
+
+let apellido
+
+apellido = 'Chavez'
+console.log(apellido)
+
+apellido = 'Rosario'
+console.log(apellido)
+
